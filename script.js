4dda0e94f2091e39f63f3f4b1872ffad493315fd
Mathieu <Mathieu.hj.anderson@gmail.com>
2016-11-10 12:31:55 +1300

    Add correct Ajax request, add button

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(function (){
-var $data = $('#quotes');
+var $quotesObject = $('#quotes');
   $.ajax({
     type: 'GET',
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies',
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
     dataType: "json",
     headers: { "X-Mashape-Key": "qzwa4lx4gVmshWzgX5hbOgznXaK2p1XBRPQjsnEBhCpIknHhcG" },
-    success: function(data) {
-      $.each(data, function(quotes){
-        $data.append('<li>test</li>');
+    success: function(quotesObject) {
+      $.each(quotesObject, function(quotesProperty){
+        $quotesObject.append('<p>' + quotesObject[quotesProperty] + '</p>');
 
       });
     }
