d7bcc6d7a5995c371e3b3501104664a94cef9bb0
MeghanHughes <hughes.meghan@gmail.com>
2016-11-10 11:59:58 +1300

    test list up

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(function (){
-
+var $data = $('#quotes');
   $.ajax({
     type: 'GET',
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies',
     dataType: "json",
     headers: { "X-Mashape-Key": "qzwa4lx4gVmshWzgX5hbOgznXaK2p1XBRPQjsnEBhCpIknHhcG" },
     success: function(data) {
-      console.log('success', data);
+      $.each(data, function(quotes){
+        $data.append('<li>test</li>');
+
+      });
     }
   });
 
