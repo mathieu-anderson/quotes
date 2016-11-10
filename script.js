dd9cc165089377076863b44fb89633edf13b9ea6
MeghanHughes <hughes.meghan@gmail.com>
2016-11-10 13:01:29 +1300

    quotes regenerating

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ var $quotesObject = $('#quotes');
   });
 
 });
+
+var buttonClick = function (){
+$('#quotes').empty();
+  $(function (){
+  var $quotesObject = $('#quotes');
+    $.ajax({
+    type: 'GET',
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+    dataType: "json",
+    headers: { "X-Mashape-Key": "qzwa4lx4gVmshWzgX5hbOgznXaK2p1XBRPQjsnEBhCpIknHhcG" },
+    success: function(quotesObject) {
+      $.each(quotesObject, function(quotesProperty){
+        $quotesObject.append('<p>' + quotesObject[quotesProperty] + '</p>');
+
+      });
+    }
+  });
+
+});
+
+};
